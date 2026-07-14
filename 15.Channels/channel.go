62875751d7771abcae5f38b29e02d602565f195e
Bilal-Ahmed4 <bilalahmedaka1@gmail.com>
2026-07-15 01:04:44 +0500

    channels

--- 15.Channels/channel.go
+++ 15.Channels/channel.go
@@ -2,14 +2,15 @@
 
 package main
 
-import "fmt"
+import ("fmt"
+"time")
 
 // func sendData(mychan chan string) {
-// 	//this channel can send and recieve data both ways if we want to make it one way then we have
-// 	// to specify the direction of the channel
-// 	// chan string is the direction of the channel
-// 	// chan<- string is the direction of the channel that can only send data
-// 	// <-chan string is the direction of the channel that can only receive data
+// 	/this channel can send and recieve data both ways if we want to make it one way then we have
+// 	/ to specify the direction of the channel
+// 	/ chan string is the direction of the channel
+// 	/ chan<- string is the direction of the channel that can only send data
+// 	/ <-chan string is the direction of the channel that can only receive data
 // 	mychan <- "go lang " //Now we are sending data to the channel
 // }
 
@@ -22,18 +23,84 @@ import "fmt"
 // }
 
 // receive
-func sum(resultChan chan int, num1, num2 int) {
-	resultChan <- num1 + num2
+// func sum(resultChan chan int, num1, num2 int) {
+// 	resultChan <- num1 + num2
+// }
+//go routine synchronization
+//for synchronization instead of wait groups
+// func task(done chan bool){
+	//defer is a function that runs in the last whether the func cleanly run or give an error
+// 	defer func(){
+// 		done <-true
+// 	}()
+// 	fmt.Println("Processing done")
+// }
+
+func emailSender(emailChan <-chan string, done chan<- bool) {
+	defer func() { done <- true }()
+
+	for email := range emailChan {
+		fmt.Println("sending email to", email)
+		time.Sleep(time.Second)
+	}
 }
 
 func main() {
-	numChan := make(chan int)
+	chan1 := make(chan int)
+	chan2 := make(chan string)
 
-	go sum(numChan, 5, 5)
+	go func() {
+		chan1 <- 10
+	}()
 
-	res := <-numChan //blocking call
+	go func() {
+		chan2 <- "pong"
+	}()
 
-	fmt.Println(res)
+	for i := 0; i < 2; i++ {
+		select {
+		case chan1Val := <-chan1:
+			fmt.Println("received data from chan1", chan1Val)
+		case chan2Val := <-chan2:
+			fmt.Println("received data from chan2", chan2Val)
+		}
+	}
+      
+	//normal channels are blocking but buffer channles allow us to send a an amount of data
+	//without blocking
+	// emailChan := make(chan string,100)//100 buffer channle like we can send 100 channels without blocking
+	// done := make(chan bool)
+	// go emailchan(emailChan,done)
+
+	// for i := 0; i < 5; i++ {
+	// 	emailChan <- fmt.Sprintf("%d@gmail.com", i)
+	// }
+ 
+	// fmt.Println("done")
+
+	//for a channel we have to use the in built close function
+	// close(emailChan)
+	// <-done
+
+
+
+
+
+
+
+
+	// numChan := make(chan int)
+	// done := make(chan bool)
+
+	// go sum(numChan, 5, 5)
+
+	// res := <-numChan //blocking call
+
+	// fmt.Println(res)
+
+	// go task(done)
+    // now we are using it as a wait group use channels for only one go if multiple use wait group for clean syntax
+	// <- done
 
 	// go processNum(numChan)
 
